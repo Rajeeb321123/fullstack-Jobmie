9d2d720b46c8ebee5c373077c2e912ec8e265b5c
Rajeeb321123 <starsgazer321@gmail.com>
2023-06-25 22:47:09 +0545

    final

--- client/src/state/index.js
+++ client/src/state/index.js
@@ -31,7 +31,7 @@ export const globalSlice = createSlice({
 })
 
 // below provide access of state and reducer in other file
-export const { setRendercom } = globalSlice.actions;
+export const { setRendercom, setRendercomGigs } = globalSlice.actions;
 
 export default globalSlice.reducer;
 // while we import state in index.js in src, we have to import globalReducer as name of this slice is globalSlice
